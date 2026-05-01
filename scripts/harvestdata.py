55bc72b09667428993f74c21ae7815f2c091a20e
Cameron Stanhope <camski3105@gmail.com>
2026-05-01 12:33:40 +0100

    1st draft of harvestdata.py

--- scripts/harvestdata.py
+++ scripts/harvestdata.py
@@ -1,1 +1,29 @@
-print("Harvesting data from yfinance and FRED API")
+import pandas as pd
+import yfinance as yfin
+
+print("Gathering ETF data from Yahoo Finance...")
+
+# Create a list of ETF tickers to identify sector proxies
+etf_tickers = ["XLK", "XLF", "XLE", "XLV", "SPY"]
+
+# Establish time range for data retrieval.
+start_date = "2010-01-01"
+end_date = "2025-12-31"
+
+for etf_ticker in etf_tickers:
+    print(f"Downloading: {etf_ticker}")
+
+    data = yfin.download(etf_ticker, start=start_date, end=end_date)
+
+    # Check for any empty datasets
+    if data.empty:
+        print(f"Failed: No data available to download for {etf_ticker}")
+        continue
+
+    # Save the data as a CSV file
+    filename = f"data/raw/{etf_ticker}.csv"
+    data.to_csv(filename)
+
+    print(f"Saved: {filename}")
+
+print("Finished downloading ETF data from Yahoo Finance")
